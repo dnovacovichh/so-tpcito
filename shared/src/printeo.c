9587d6f41c4430616966c3550d95a3c01392c28a
Dani Nova <dnovacovich@frba.utn.edu.ar>
2024-04-01 05:52:45 +0000

    First version

---
+++ shared/src/printeo.c
@@ -0,0 +1,9 @@
+#include "../include/printeo.h"
+
+void print_hola(char* nombre){
+    printf("Hola, como andas, %s?/n", nombre);
+}
+
+void print_chau(char* nombre){
+    printf("Chau %s?/n", nombre);
+}
